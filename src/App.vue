04bc8a1e7c34c12ac114ecc3fba2277c7b397e88
Neketa <nekit53@gmail.com>
2022-01-14 17:22:57 +0300

    add folders

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="main">
+        <div class="header">
+            <input type="text">
+        </div>
+        <ul class="todo-list">
+            <li class="todo-item"></li>
+            <li class="todo-item"></li>
+            <li class="todo-item"></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+          return {
+
+          }  
+        }
+    }
+</script>
+
+<style>
+
+</style>
